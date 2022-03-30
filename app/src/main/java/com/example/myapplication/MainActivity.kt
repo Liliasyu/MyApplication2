78eb665a5d4a87322a6ae8d842ffab9ca81a8e6c
ii <ii@gmail.com>
2022-03-30 14:15:32 +0800

    2.i am here now ok i saw you
    test.project 111 03 30
    測時集拉

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var myii:TextView=findViewById<TextView>(R.id.ii)
         myii.text="ok i saw you"
+
+        var butt=findViewById<Button>(R.id.button)
+        var butt2=findViewById<Button>(R.id.button2)
     }
 }
