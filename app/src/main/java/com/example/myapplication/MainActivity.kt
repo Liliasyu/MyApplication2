3d750d851e34daa2157c205bfabd0ac97fc8caae
ii <ii@gmail.com>
2022-03-30 14:06:05 +0800

    i am here now ok i saw you
    test.project 111 03 30
    測時集拉

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var myii:TextView=findViewById<TextView>(R.id.ii)
+        myii.text="ok i saw you"
+    }
+}
